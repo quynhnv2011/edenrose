02f0bc019364941951ea9696e9f158e157b9dbc5
quynhnv <quynhnv@datacloud.vn>
2018-05-03 10:54:30 +0700

    update

--- Edenrose.web/Controllers/TinTucController.cs
+++ Edenrose.web/Controllers/TinTucController.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Vks.Common.Utils;
@@ -79,6 +81,43 @@ namespace Edenrose.web.Controllers
                     CreatedDate = DateTime.Now
                 };
                 var result = _resignService.Add(obj);
+
+                //gửi email về quản trị
+
+                try
+                {
+
+                    var body = new StringBuilder();
+                    body.AppendFormat("<p>Kính gửi quản trị viên website Edenrose.vn,{0}</p>", Environment.NewLine);
+                    body.AppendFormat("<p>Hệ thống website Edenrose.vn vừa nhận được dữ liệu đăng kí với thông tin:{0}</p>", Environment.NewLine);
+                    body.AppendFormat("<p>- Họ và tên: {0}.{1}</p>", obj.Name, Environment.NewLine);
+                    body.AppendFormat("<p>- Số điện thoại: {0}</p>", obj.Phone);
+                    body.AppendFormat("<p>- Email: {0}</p>", obj.Email);
+                    body.AppendFormat("<p>- Thời gian: {0}.{1}</p>", DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy"), Environment.NewLine);
+
+                    body.AppendFormat("<p>Trân trọng.{0}</p>", Environment.NewLine);
+                    body.AppendFormat("<p><i>Đây là Email gửi đăng ký tự động, vui lòng không reply. Xin cảm ơn!</i></p>");
+
+                    MailMessage mail = new MailMessage();
+                    mail.To.Add("hai38e5@gmail.com");
+                    mail.CC.Add("edenrosehn@gmail.com");
+                    mail.From = new MailAddress("edenrosehn@gmail.com");
+                    mail.Body = body.ToString();
+                    mail.Subject = "Thông tin khách hàng đăng ký dự án Edenrose";
+                    mail.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new System.Net.NetworkCredential("edenrosehn@gmail.com", "eden@123"); // Enter seders User name and password  
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                    OutputLog.WriteOutputLog(ex);
+                }
+
                 var FolderPath = System.Web.HttpContext.Current.Request.MapPath(string.Format("/BangGia"));
 
                 //Define file Type
